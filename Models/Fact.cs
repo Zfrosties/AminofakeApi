072ad2c2cb4b1d961254c41c51a9d98591b0d9b5
Zfroties <degreezbeatz@gmail.com>
2017-10-01 21:40:24 +0200

    first commit

---
+++ Models/Fact.cs
@@ -0,0 +1,15 @@
+using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Threading.Tasks;  
+
+namespace aminofakeApi.Models { 
+   public class Fact { 
+    public string title { get; set; } 
+    public string content { get; set;} 
+    public bool validated { get; set;} 
+
+    public bool fake {get; set;}
+
+   } 
+}
